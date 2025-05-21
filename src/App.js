57523bf1ac03dceba26e9b00bc613851db398fc2
Yole <igbavagodswill4@gmail.com>
2025-05-21 20:23:01 +0100

    Imported all images to the app component currently working on the static content of the app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// imported app images
+import divider1 from "./assets/pattern-divider-mobile.svg";
+import divider2 from "./assets/pattern-divider-desktop.svg";
+import dice from "./assets/icon-dice.svg";
+
 export default function App() {
   return <AdviceGenerator />;
 }
@@ -11,5 +16,20 @@ function AdviceGenerator() {
 }
 
 function AdviceContainer() {
-  return;
+  return (
+    <section className="advice-container">
+      <p className="container-heading">advice</p>
+      <Advice />
+    </section>
+  );
+}
+
+function Advice() {
+  return (
+    <p className="advice">
+      <span>"</span>
+      advice
+      <span>"</span>
+    </p>
+  );
 }
